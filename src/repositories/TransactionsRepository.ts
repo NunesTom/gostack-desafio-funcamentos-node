b75e4c86ac57c97888bd2f8229f2b314f0558c0e
Ailton Nunes <ailton.souza@firstdecision.com.br>
2020-05-06 20:45:52 -0300

    Desafio 6

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,19 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
 }
 
 class TransactionsRepository {
@@ -14,15 +24,49 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (accum, curr) => {
+        if (curr.type === 'income') {
+          accum.income += curr.value;
+        } else if (curr.type === 'outcome') {
+          accum.outcome += curr.value;
+        } else {
+          throw Error('Type is not valid');
+        }
+
+        return accum;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    if (type === 'outcome') {
+      const curr = this.getBalance();
+      const checkBalance = curr.income - (curr.outcome + value);
+
+      if (checkBalance < 0) {
+        throw Error('Value of outcome is insufficient');
+      }
+    }
+
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
